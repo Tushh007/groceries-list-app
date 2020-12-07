dd6ab79ae9de03d2eedc68cb15544c931915fc5f
Tushar Langer <tushar.langer@joulon.com>
2020-12-07 16:38:56 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { createList } from "./lists";
+import { setFilters } from "./filters";
+import { renderLists } from "./views";
+
+renderLists();
+
+document.querySelector("#create-list").addEventListener("click", () => {
+  const id = createList();
+  location.assign(`/edit.html#${id}`);
+});
+
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  setFilters({
+    searchText: e.target.value,
+  });
+  renderLists();
+});
+
+document.querySelector("#filter-by").addEventListener("change", (e) => {
+  setFilters({
+    sortBy: e.target.value,
+  });
+  renderLists();
+});
+
+window.addEventListener("storage", (e) => {
+  if (e.key === "lists") {
+    renderLists();
+  }
+});
